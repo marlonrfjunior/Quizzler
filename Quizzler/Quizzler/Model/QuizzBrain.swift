c48b16b055e898f908f57d585c3f528f6efaf4ec
Marlon <marlonrfjunior@outlook.com>
2021-08-19 00:00:12 -0300

    Implementando estrutura MVC

---
+++ Quizzler/Quizzler/Model/QuizzBrain.swift
@@ -0,0 +1,30 @@
+//
+//  QuizzBrain.swift
+//  Quizzler
+//
+//  Created by Marlon Junior🦈⚓️ on 18/08/21.
+//
+
+import Foundation
+
+struct QuizzBrain {
+  
+    var quetionNumber: Int = 0
+
+    let quiz = [
+        question(question: "O monte Everest cresce 4 milímetros por ano.", awnser: "True"),
+        question(question: "Existe uma lâmpada que está ligada há mais de 116 anos na cidade de Livermore, na Califórnia.", awnser: "False"),
+        question(question: "O cérebro humano é formado, basicamente, por 75% de água.", awnser: "True"),
+        question(question: "Vacas não consegue descer escadas.", awnser: "True"),
+        question(question: "4% da população mundial é canhota.", awnser: "False"),
+        question(question: "Brasil significa 'vermelho como brasa'", awnser: "True"),
+        question(question: "O Brasil é o maior país do mundo..", awnser: "False"),
+        question(question: "o Brasil é o maior produtor de café do mundo.", awnser: "True"),
+        question(question: "Tocantins, fundado em 1938, é o estado mais novo do Brasil.", awnser: "False"),
+        question(question: "Na Holanda o número de bicicletas ultrapasse três vezes o número de veículos.", awnser: "True")
+        
+    ]
+    
+}
+
+
